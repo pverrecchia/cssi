3fb5954962b7e999ba1a2b50bfd8963b4374f856
Juliana Nazare <jules.nazare@gmail.com>
2013-07-09 21:30:05 -0400

    multi_color

--- Exercises/multi_color/multi_color.ino
+++ Exercises/multi_color/multi_color.ino
@@ -1,17 +1,17 @@
-//Exercise 3: Photoresistor and Multi-Color LED
-//Google CSSI Arduino Workshop
-//Cambridge, Massachusetts
-//July 26, 2013
+// Exercise 3: Photoresistor and Multi-Color LED
+// Google CSSI Arduino Workshop
+// Cambridge, Massachusetts
+// July 26, 2013
 
                                                
-int lightValueresistor = 12;                            //Pin declarations
+int lightValueresistor = 12;                            // Pin declarations
 int redPin = 11;             
 int greenPin = 10;           
 int bluePin = 9;             
 
 void setup()
 {
-    Serial.begin(9600);                                 //Begin serial communcation
+    Serial.begin(9600);                                 // Begin serial communcation
     
     pinMode(redPin, OUTPUT);      
     pinMode(greenPin, OUTPUT);
@@ -21,45 +21,47 @@ void setup()
 
 void loop()
 {
-    Serial.println(analogRead(lightValueresistor));     //print lightValueresistor value to console
-    setColor(analogRead(lightValueresistor));           //set LED color based on measured brightness
+    Serial.println(analogRead(lightValueresistor));     // Print lightValueresistor value to console
+    setColor(analogRead(lightValueresistor));           // Set LED color based on measured brightness
                                                 
-    delay(10);                                          //delay 10 milliseconds
+    delay(10);                                          // Delay 10 milliseconds
 }
 
 void setColor(int lightValue)
 {
-  if(lightValue >= 213){                                //RED
+
+// Switch statement to make the LED alternate between colors (using RGB values) 
+// according to the light value of the photoresistor
+
+// If the light value is greater than 213, set the LED to RED
+  if(lightValue >= 213){         
     red = 255;
     blue = 0;
     green = 0;
-  }else if(lightValue < 213 && lightValue >= 170){      //YELLOW
-    red = 255;
-    blue = 0;
-    green = 255;
-  }else if(lightValue < 170 && lightValue >= 128){      //GREEN
-    red = 0;
-    blue = 0;
-    green = 255;
-  }else if(lightValue < 128 && lightValue >= 86){       //TURQUOISE
-    red = 0;
-    blue = 255;
-    green = 255;
-  }else if(lightValue < 86 && lightValue >= 43){        //BLUE
-    red = 0;
-    blue = 255;
-    green = 0;
-  }else if(lightValue < 86 && lightValue >= 43){        //PURPLE
-    red = 255;
-    blue = 255;
-    green = 0;
-  }else if(lightValue < 43){                            //WHITE
-    red = 255;
-    blue = 255;
-    green = 255;
+
   }
+
+// Else if the light value is between 170 and 213, set the LED to YELLOW
+  else if(lightValue < 213 && lightValue >= 170){
+
+  }
+
+// Else if the light value is between 128 and 170, set the LED to GREEN
+  else if(lightValue < && lightValue >=){ 
+
+  }
+
+// Else if the light value is between 86 and 128, set the LED to TURQUOISE (set blue and green to high)
+  else if(){
+
+  }
+
+// Else if the light value is between 43 and 86, set the LED to BLUE
+
+// Else if the light value is less than 43, set the LED to PURPLE
   
-  analogWrite(redPin, red);                             //write RGB values to LED
+// Write RGB values to LED
+  analogWrite(redPin, red);
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);  
 }
