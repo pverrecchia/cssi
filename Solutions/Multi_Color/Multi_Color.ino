2248269abd5475f0f68cb8502a3068d48cc1b5fc
Pietro Verrecchia <pverrecchia@gmail.com>
2013-07-23 22:24:10 -0400

    piedro multi color

--- Solutions/Multi_Color/Multi_Color.ino
+++ Solutions/Multi_Color/Multi_Color.ino
@@ -4,60 +4,62 @@
 //July 26, 2013
 
                                                
-int lightValueresistor = 12;                            //Pin declarations
+int lightValueResistor = 0;                            //Pin declarations
 int redPin = 11;             
 int greenPin = 10;           
-int bluePin = 9;             
+int bluePin = 9;    
+int red, green, blue, v = 0;
 
 void setup()
 {
-    Serial.begin(9600);                                 //Begin serial communcation
+    Serial.begin(9600);                                
     
     pinMode(redPin, OUTPUT);      
     pinMode(greenPin, OUTPUT);
     pinMode(bluePin, OUTPUT);
-    pinMode(lightValueresistor, INPUT);  
+    pinMode(lightValueResistor, INPUT);  
 }
 
 void loop()
 {
-    Serial.println(analogRead(lightValueresistor));     //print lightValueresistor value to console
-    setColor(analogRead(lightValueresistor));           //set LED color based on measured brightness
+    v = analogRead(lightValueResistor);  //measure photo resistor
+    Serial.println(v);                   //print measured value to console
+    setColor(v);                         //set LED color based on measured brightness
                                                 
-    delay(10);                                          //delay 10 milliseconds
+    delay(10);                            
 }
 
 void setColor(int lightValue)
 {
-  if(lightValue >= 213){                                //RED
-    red = 255;
+  if(lightValue >= 700){                                //RED
+    red = 200;
     blue = 0;
     green = 0;
-  }else if(lightValue < 213 && lightValue >= 170){      //YELLOW
-    red = 255;
+  }else if(lightValue < 700 && lightValue >= 600){      //YELLOW
+    red = 200;
     blue = 0;
-    green = 255;
-  }else if(lightValue < 170 && lightValue >= 128){      //GREEN
+    green = 200;
+  }else if(lightValue < 600 && lightValue >= 500){      //GREEN
     red = 0;
     blue = 0;
-    green = 255;
-  }else if(lightValue < 128 && lightValue >= 86){       //TURQUOISE
+    green = 200;
+  }else if(lightValue < 500 && lightValue >= 400){       //TURQUOISE
     red = 0;
-    blue = 255;
-    green = 255;
-  }else if(lightValue < 86 && lightValue >= 43){        //BLUE
+    blue = 200;
+    green = 200;
+  }else if(lightValue < 400 && lightValue >= 300){        //BLUE
     red = 0;
-    blue = 255;
+    blue = 200;
     green = 0;
-  }else if(lightValue < 43){                            //PURPLE
-    red = 255;
-    blue = 255;
+  }else if(lightValue < 300){                            //PURPLE
+    red = 200;
+    blue = 200;
     green = 0;
   }
   
-  analogWrite(redPin, red);                             //write RGB values to LED
+  //write RGB values to LED
+  analogWrite(redPin, red);                           
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);  
 }
-
 
