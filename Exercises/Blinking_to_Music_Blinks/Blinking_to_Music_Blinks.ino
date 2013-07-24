005cb08ee7f09645a8502db2306ccf73683887a8
Juliana Nazare <jules.nazare@gmail.com>
2013-07-23 22:16:27 -0400

    exercise code for blinking to music

--- Exercises/Blinking_to_Music_Blinks/Blinking_to_Music_Blinks.ino
+++ Exercises/Blinking_to_Music_Blinks/Blinking_to_Music_Blinks.ino
@@ -1,79 +1,103 @@
-/*
-blinks: 1, 2, 1&2
- */
+// NOTE: Global variables
 boolean on = false;
-int blink_count = 0;
+int currentState = 0;
 int count = 0;
-int ceiling = 50;
-
+int ceiling;
+int sensorValue;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(13, OUTPUT); //blue
-  pinMode(12, OUTPUT); //yellow
-  pinMode(11, OUTPUT); //red?
+  // NOTE: Initialize the pins and set to OUTPUT mode
+  pinMode(12, OUTPUT); // Green
+
+  // DO: Set pin 11 to OUTPUT MODE
+  pinMode(11, ); // Blue
+
+  // DO: Set pin 10 to OUTPUT MODE
+  pinMode( , ); // Red
+  ceiling = 25;
 }
 
+// NOTE: This is the loop function that runs when the Ardunio runs
 void loop() {
-  //int ceiling = round(analogRead(A1)/10.24);
-  int ceiling = 25;
-  delay(2.5);
-  
-  int sensorValue = analogRead(A0);
-   Serial.println(sensorValue);
-  Serial.print(on);
-  if (sensorValue > ceiling && !on)
+
+  // NOTE: The analog read function get the voltage being output from the speaker
+  sensorValue = analogRead(A0);
+
+  // NOTE: If statement to check: if the lights are not on and the music is getting louder
+  // DO: Write the if statement to check if: sensorValue > ceiling and !on (!on already written)
+  if (( ) && !on)
   {
+    // NOTE: Set flag that indicates if lights are on to true
     on = true;
-    
-    if (count > 20)
+
+    // NOTE: We want to keep track of the number of samples we have taken
+    // NOTE: Once 20 samples are taken, change the lights
+    // DO: Set if statement to: if count is greater than 20
+    if (count >  )
     {
-   
-      if (blink_count <= 2)
+      
+      // NOTE: Increment the state if the state is less than 2
+      // DO: Set if statement to: if currentState is less than or equal to 2
+      if (currentState )
       {
-        Serial.print("here");
-        blink_count ++;
+        // DO: Increment the currentState variable by one each iteration
       }
+      
+      // NOTE: The code enters this loop when we are at the last state (3)
       else
       {
-         blink_count = 0;
+        // NOTE: Set currentState to 0 in last state
+        currentState = 0;
       }
 
-      
-      count = 0;
-      
-    }
-    
-    //mMSerial.print(blink_count);
+      // NOTE: Resets the number of samples at the end of the loop
 
-    if(blink_count == 0)
-    {
-      Serial.print(count);
-      digitalWrite(13, HIGH);
-      Serial.print("b0");
-      count ++;
+      // DO: Set count equal to zero
     }
-    else if (blink_count == 1)
-    {
-      digitalWrite(12, HIGH);
-      Serial.print("b1");
-      count ++;
+
+
+    // NOTE: Switch statements to change the lights
+    switch(currentState){
+
+      // NOTE: Turn light at pin 12 on to HIGH
+      case 0:
+        digitalWrite(12, HIGH);
+        break;
+
+      // DO: Turn light at pin 11 on to HIGH
+      case 1:
+        digitalWrite(11, );
+        break;
+
+      // DO: Turn light at pin 10 on to HIGH
+      case 2:
+
+      // NOTE: Default to exit loop
+      default:
+        break;
     }
-    else if (blink_count == 2)
-    {
-      digitalWrite(11, HIGH);
-      Serial.print("b2");
-      count ++;
-    }
+
+    // NOTE: Increment the count after every loop
+    count ++;
   }
-  else if (sensorValue < ceiling && on)
-  {
-    on = false;
-    
-    digitalWrite(13, LOW);
-    digitalWrite(12, LOW);
-    digitalWrite(11, LOW);
-  }
+
+  // NOTE: This if statment is entered when the music gets softer
   
+  // DO: Create else if statement that is entered in the sensorValue is less than the ceiling and on == true
+  else if ( )
+  {
+    // NOTE: Set flag that indicates if lights are on to false
+    on = false;
+
+    // NOTE: Set the LED at pin 12 to LOW
+    digitalWrite(12, LOW);
+
+    // DO: Set the LED at pin 11 to LOW
+    digitalWrite( , );
+
+    // DO: Set the LED at pin 10 to LOW
+  }
+
+  // NOTE: Added a delay of 2.5 milliseconds
   delay(2.5);
 }
